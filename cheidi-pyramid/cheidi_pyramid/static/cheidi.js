33495a3a7d207482e120221e96113b28a59ab0dd
Adam Castle <acastle@ripe.net>
2016-03-20 17:25:48 +0100

    homepage done

--- cheidi-pyramid/cheidi_pyramid/static/cheidi.js
+++ cheidi-pyramid/cheidi_pyramid/static/cheidi.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
-    var num = 30; //number of pixels before modifying styles
 
-    var pos_left = $('#menu').offset().left;
-    $(window).bind('scroll', function () {
-        if ($(window).scrollTop() > num) {
-            $('#menu').addClass('fixed');
-
-        } else {
-            $('#menu').removeClass('fixed');
-        }
-    });
+    var easter_egg = new Konami();
+    easter_egg.load('https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xtp1/v/t1.0-9/10945670_10153112801843401_803602855809665036_n.jpg?oh=38527a8de28be30d0281a8e57699b6f5&oe=578EDFAA&__gda__=1468148620_7b8d8671a54156d8e2670ca2f39822dd');
 
 });
